b4cf625668563d7f4beb6dffb7a214ab1ce7155a
thanh chung <nguyenthanhchung@atlantic.edu.vn>
2024-12-09 18:30:58 +0700

    init provider app, logo, breadcrumHeader

---
+++ components/BreadcrumbHeader.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+} from "@/components/ui/breadcrumb";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const BreadcrumbHeader = () => {
+  const pathName = usePathname();
+  const paths = pathName === "/" ? [""] : pathName?.split("/");
+  return (
+    <div className="flex items-center">
+      <Breadcrumb>
+        <BreadcrumbList>
+          {paths.map((path, index) => (
+            <React.Fragment key={index}>
+              <BreadcrumbItem>
+                <BreadcrumbLink className="capitalize" href={`/${path}`}>
+                  {path === "" ? "home" : path}
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </React.Fragment>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+};
+
+export default BreadcrumbHeader;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import Logo from "@/components/Logo";
+import { buttonVariants } from "@/components/ui/button";
 import {
   CoinsIcon,
   HomeIcon,
   Layers2Icon,
   ShieldCheckIcon,
 } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const routes = [
@@ -32,9 +36,34 @@ const routes = [
 ];
 
 const DestopSidebar = () => {
+  const pathName = usePathname();
+  const activeRoute =
+    routes.find(
+      (route) => route.href.length > 0 && pathName.includes(route.href)
+    ) || routes[0];
   return (
     <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
-      <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4"></div>
+      <div className="flex items-center justify-center gap-2 border-b-[1px] border-separate p-4">
+        <Logo />
+      </div>
+      <div className="p-2">TODO CREDITS</div>
+      <div className="flex flex-col gap-1 p-2">
+        {routes.map((route, index) => (
+          <Link
+            key={route.href + index}
+            href={route.href}
+            className={buttonVariants({
+              variant:
+                activeRoute.href === route.href
+                  ? "sidebarActiveItem"
+                  : "sidebarItem",
+            })}
+          >
+            <route.icon size={20} />
+            {route.label}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
